3cb7005b9e14da22a6a3748283e902d31aad1a68
roy osherove <roy@osherove.com>
2013-03-07 10:55:06 +0100

    added tests for functions.cs

--- StringCalculatorSamples/StringCalculatorTests/Functions.cs
+++ StringCalculatorSamples/StringCalculatorTests/Functions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using NUnit.Framework;
 
 namespace StringCalculatorApp
 {
@@ -55,11 +56,104 @@ namespace StringCalculatorApp
             var negatives = numbers.Where(number => number < 0).ToArray();
             if (negatives.Any())
             {
-                throw new ArgumentException(string.Format("negatives not allowed: {0}", negatives));
+                throw new ArgumentException(string.Format("negatives not allowed: [{0}]", string.Join(",",negatives)));
             }
 
             // Sum and return!
             return numbers.Sum();
         }
+
     }
+
+    [TestFixture]
+    public class StringCalculatorTestAfter
+    {
+        StringCalculator calculator = new StringCalculator();
+
+        [Test]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            int result = calculator.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Add_OneNumber_ReturnsNumber()
+        {
+            int result = calculator.Add("42");
+
+            Assert.AreEqual(42, result);
+        }
+
+        [Test]
+        public void Add_TwoNumbersDelimitedWithComma_ReturnsSum()
+        {
+            int result = calculator.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Add_MultipleNumbersDelimitedWithComma_ReturnsSum()
+        {
+            int result = calculator.Add("1,2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void Add_TwoNumbersDelimitedWithNewLine_ReturnsSum()
+        {
+            int result = calculator.Add("1\n2");
+
+            Assert.AreEqual(3, result);
+        }
+
+
+        [Test]
+        public void Add_TwoNumbersDelimitedWithCustomDelimiter_ReturnsSum()
+        {
+            string input = "//;\n1;2";
+            int result = calculator.Add(input);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_NegativeNumber_ThrowsArgumentException()
+        {
+            calculator.Add("-1");
+        }
+
+        [Test]
+        public void Add_NegativeNumber_ErrorMessageContainsNumber()
+        {
+            try
+            {
+                calculator.Add("-1");
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("negatives not allowed: [-1]", e.Message);
+            }
+        }
+
+        [Test]
+        public void Add_MultipleNegativeNumbers_ErrorMessageContainsAllNegativeNumbers()
+        {
+            try
+            {
+                calculator.Add("-1,2,-3");
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("negatives not allowed: [-1,-3]", e.Message);
+            }
+        }
+    }
+
 }
