01d8ca169f23b7725e8be5ebea69c35e7bc5c943
roy osherove <roy@osherove.com>
2013-03-08 05:12:56 +0100

    extrat SRP DayReport

---
+++ GildedRose/DayReporter.cs
@@ -0,0 +1,18 @@
+﻿namespace GildedRose.Console
+{
+    public class DayReporter
+    {
+        public void Report(int dayNumber, Program app)
+        {
+
+            System.Console.WriteLine("-------- day " + dayNumber + " --------");
+            System.Console.WriteLine("name, sellIn, quality");
+            for (var j = 0; j < app.Items.Count; j++)
+            {
+                System.Console.WriteLine(app.Items[j].Name + ", " + app.Items[j].SellIn + ", " + app.Items[j].Quality);
+            }
+            System.Console.WriteLine("");
+
+        }
+    }
+}
--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -4,57 +4,66 @@ namespace GildedRose.Console
 {
     public class Program
     {
-        IList<Item> Items;
         public static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 10,
-                                                      Quality = 49
-                                                  },
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 5,
-                                                      Quality = 49
-                                                  },
-                                              // this conjured item does not work properly yet
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
+            var app = new Program(new DayReporter(),GetItems())
+            app.Run();
+        }
 
-                          };
+        public IList<Item> Items;
+        public DayReporter Reporter { get; set; }
 
+        private Program(DayReporter dayReporter, List<Item> items)
+        {
+            Reporter = dayReporter;
+            Items = items;
+        }
+
+
+        public void Run()
+        {
             for (var i = 0; i < 31; i++)
             {
-                System.Console.WriteLine("-------- day " + i + " --------");
-                System.Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < app.Items.Count; j++)
-                {
-                    System.Console.WriteLine(app.Items[j].Name + ", " + app.Items[j].SellIn + ", " + app.Items[j].Quality);
-                }
-                System.Console.WriteLine("");
-                app.UpdateQuality();
+                Reporter.Report(i, this);
+                UpdateQuality();
             }
-
         }
+
+        private static List<Item> GetItems()
+        {
+            return new List<Item>
+                       {
+                           new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                           new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                           new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                           new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                           new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                           new Item
+                               {
+                                   Name = "Backstage passes to a TAFKAL80ETC concert",
+                                   SellIn = 15,
+                                   Quality = 20
+                               },
+                           new Item
+                               {
+                                   Name = "Backstage passes to a TAFKAL80ETC concert",
+                                   SellIn = 10,
+                                   Quality = 49
+                               },
+                           new Item
+                               {
+                                   Name = "Backstage passes to a TAFKAL80ETC concert",
+                                   SellIn = 5,
+                                   Quality = 49
+                               },
+                           // this conjured item does not work properly yet
+                           new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                       };
+        }
+
+
 
         public void UpdateQuality()
         {
